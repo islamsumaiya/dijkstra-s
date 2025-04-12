340aad57a7232666b399df5cc9e0d4e933b462d2
Sumaiya Islam Sumi <sumiislamsumaiya@gmail.com>
2025-04-12 17:37:11 +0600

    Create dijkstra's.cpp

---
+++ dijkstra's.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<utility>
+#define INF 100000
+using namespace std;
+void addEdge(vector<pair<int,int>>adj[],int u,int v,int wt)
+{
+    adj[u].push_back(make_pair(v,wt));
+    adj[v].push_back(make_pair(u,wt));
+}
+void shortestPath(vector<pair<int,int>>adj[],int v, int src)
+{
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+    vector<int>dist(v,INF);
+    push_back(make_pair(0,src));
+    dist[src]=0;
+    while(!pq.empty())
+    {
+        int u=pq.top().second;
+        pq.pop();
+        for(auto x:adj[u])
+        {
+            int v=x.first;
+            int weight =x.second;
+            if(dist[v]>dist[u]+weight)
+            {
+                dist[v]=dist[u]+weight;
+                pq.push(make_pair(dist[v],v));
+            }
+        }
+    }
+    printf("Vertex Distance from Source\n");
+    for(int i=0; i<V; ++i)
+        printf("%d \t\t %d\n",i,dist[i]);
+
+}
+int main()
+{
+    int V=4;
+    vector<pair<int,int>>adj[V];
+    addEdge(adj,0,1,4);
+    addEdge(adj,0,2,2);
+    addEdge(adj,0,3,5);
+    addEdge(adj,2,3,1);
+    shortestPath(adj,V,0);
+    return 0;
+}
